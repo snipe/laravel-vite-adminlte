dd386446e8065df01bff94c0efb63c8aad7430a3
snipe <snipe@snipe.net>
2024-06-01 17:58:47 +0100

    Small mod to content width
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -14,7 +14,17 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <p>
+                        {{ __('You are logged in!') }}
+                    </p>
+
+                        <x-secondary-button>
+                            Click Me if you think components are fucking stupid
+                        </x-secondary-button>
+
+                        <x-secondary-button>
+                            Click Me again if you think components are fucking stupid
+                        </x-secondary-button>
                 </div>
             </div>
         </div>
